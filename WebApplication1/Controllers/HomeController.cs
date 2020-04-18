f22318ccd37bcca3e3f61e7ed2d2feb1ce2932fc
Rajivkumar Ashokan <rajivkumar@ad.infosys.com>
2020-04-18 22:15:00 +0530

    first commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : ApiController
+    {
+        private HttpClient client = new HttpClient();
+        // GET api/<controller>
+        public async Task<IEnumerable<string>> Get()
+        {
+            var res = await client.GetAsync("http://localhost:60457/");
+            return new string[] { "value1", "value2" };
+        }        
+    }
+}
---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : ApiController
+    {
+        private HttpClient client = new HttpClient();
+        // GET api/<controller>
+        public async Task<IEnumerable<string>> Get()
+        {
+            var res1 = await client.GetAsync("http://localhost:60498/");
+            var res2 = await client.GetAsync("http://localhost:60531/");
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
